4d31276c27929100d003ee6ba5256fb4d916bd25
Shohrux <shohruxergashev1204@gmail.com>
2022-07-05 10:31:07 +0500

    if

--- bin/if02.dart
+++ bin/if02.dart
@@ -6,20 +6,16 @@
         a: integer
     Returns:
         a: a increased by 1 if positive, else decreased by 2.*/
-int func(int first,second,third){
-    if(first<second){
-        if(first<third){
-            return first;
-        }
-    else
-            return third;
-    }else
-    if(third<second){
-        return second;
-    }else{
-        return third;
-    }
-    }
-void main(){
-    print(func(10,15,1));
+
+bool func(text) {
+  int x = text.length;
+  if (x % 4== 0) {
+    return true;
+  }
+  return false;
+}
+
+void main() {
+  // write your code here
+  print(func("shohrux"));
 }
